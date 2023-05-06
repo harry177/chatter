7f4771cdab0b58866329c009364a2af180d8d97e
Harry177 <harry177@rambler.ru>
2023-05-06 05:45:44 +0200

    feat: check get-method of mongodb

--- src/Components/ChatInput/ChatInput.tsx
+++ src/Components/ChatInput/ChatInput.tsx
@@ -8,15 +8,30 @@ interface IStorage {
 export const ChatInput: React.FC<IStorage> = ({ setStorage }) => {
   const initialStack: string[] = JSON.parse(localStorage.getItem('stack') || '[]');
   const [state, setState] = useState('');
+  const [users, setUsers] = useState('');
 
   const handleInput = (event: ChangeEvent<HTMLInputElement>) => {
     setState(event.target.value);
   };
 
+  async function getUsers() {
+    const response = await fetch('http://localhost:3000/api/users', {
+      method: 'GET',
+      headers: { Accept: 'application/json' },
+    });
+
+    if (response.ok === true) {
+      const users = await response.json();
+      console.log(users[0].name);
+      setUsers(users[0].name);
+    }
+  }
+
   const handleClick = () => {
     initialStack.push(state);
     localStorage.setItem('stack', JSON.stringify(initialStack));
     setStorage(state);
+    getUsers();
   };
 
   return (
@@ -25,6 +40,7 @@ export const ChatInput: React.FC<IStorage> = ({ setStorage }) => {
       <button type="submit" onClick={handleClick}>
         Send
       </button>
+      <div>{users} - aha</div>
     </>
   );
 };
