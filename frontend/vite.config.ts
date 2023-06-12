b3daeac19d568530da2cd80849fadfd12912a6a7
Harry177 <harry177@rambler.ru>
2023-06-12 21:30:49 +0200

    refactor: change vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: './dist/index.html',
-    },
-  },
   server: {
     proxy: {
       '/socket.io/': {
