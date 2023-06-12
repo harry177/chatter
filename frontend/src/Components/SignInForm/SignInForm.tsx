11816d77845d17905eb7964700dac930c10b22ec
Harry177 <harry177@rambler.ru>
2023-06-12 04:47:33 +0200

    feat: updating react-dom dependency for main package.json

--- frontend/src/Components/SignInForm/SignInForm.tsx
+++ frontend/src/Components/SignInForm/SignInForm.tsx
@@ -30,7 +30,7 @@ export const SignInForm: React.FC<IProps> = ({ setProps, dispatchBack }) => {
   };
 
   const checkUser = async () => {
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('/api/login', {
       method: 'POST',
       headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
       body: JSON.stringify({
