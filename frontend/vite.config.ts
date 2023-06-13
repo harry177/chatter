e3522765f1b5731be575c422effc2e6db828a822
Harry177 <harry177@rambler.ru>
2023-06-13 03:10:05 +0200

    fix: change rollupoptions input path to backend server.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     manifest: true,
     rollupOptions: {
       // overwrite default .html entry
-      input: '/dist/index.html',
+      input: '/backend/dist/server.js',
     },
   },
   server: {
