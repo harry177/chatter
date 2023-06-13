d081246d8b2320b72cd181818242dbb6b09d3980
Harry177 <harry177@rambler.ru>
2023-06-13 03:03:14 +0200

    feat: add rollupoptions to vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/dist/assets/index-a13dd5e7.js',
+    },
+  },
   server: {
     proxy: {
       '/socket.io/': {
