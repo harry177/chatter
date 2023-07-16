221e490533f6ddcd6091c31a560f9470c0d55d2a
Harry177 <harry177@rambler.ru>
2023-07-17 01:59:16 +0200

    feat: complete sign up form popover

--- frontend/src/Components/SignUpPopover/SignUpPopover.tsx
+++ frontend/src/Components/SignUpPopover/SignUpPopover.tsx
@@ -4,9 +4,11 @@ import { Popover } from 'react-bootstrap';
 import './SignUpPopover.styles.scss';
 
 const popover = (
-  <Popover id="popover-basic">
-    <Popover.Header as="h3">Popover right</Popover.Header>
-    <Popover.Body>Just confirmation text</Popover.Body>
+  <Popover id="popover-basic" className="popover-main">
+    <Popover.Header as="h3" className="popover-header">
+      Important!
+    </Popover.Header>
+    <Popover.Body>Just confirm and keep calm</Popover.Body>
   </Popover>
 );
 
