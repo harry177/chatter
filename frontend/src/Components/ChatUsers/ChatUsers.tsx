f0ba9eb8d9a002187785f81bc0fc26c7c47a90b4
Harry177 <harry177@rambler.ru>
2023-07-31 04:38:53 +0200

    feat: add useMediaQuery for ChatUsers, set responsive and adaptive styles for tablet resolution

--- frontend/src/Components/ChatUsers/ChatUsers.tsx
+++ frontend/src/Components/ChatUsers/ChatUsers.tsx
@@ -1,4 +1,5 @@
 import React, { memo, useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { motion, Reorder, useDragControls } from 'framer-motion';
 import { UserItem } from '../UserItem/UserItem';
 import { socket } from '../../socket';
@@ -14,6 +15,9 @@ interface IChatUsers {
 export const ChatUsers: React.FC<IChatUsers> = memo(
   ({ dispatchChatState, chatSpeaker, online, user }) => {
     const [allUsers, setAllUsers] = useState<string[]>([]);
+    const [dropdown, setDropdown] = useState(false);
+
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 767px)' });
 
     useEffect(() => {
       socket.emit('getAll');
@@ -41,43 +45,50 @@ export const ChatUsers: React.FC<IChatUsers> = memo(
     const controls = useDragControls();
 
     return (
-      <div className="chat-users">
-        <div className="chat-users__inner">
-          <div className="chat-users__inner-container">
-            <Reorder.Group
-              as="div"
-              axis="y"
-              layoutScroll
-              values={allUsers}
-              onReorder={setAllUsers}
-              style={{ listStyleType: 'none', width: '100%' }}
-            >
-              {allUsers &&
-                allUsers.map((user) => {
-                  return (
-                    <Reorder.Item
-                      key={user}
-                      value={user}
-                      dragControls={controls}
-                      dragListener={false}
-                    >
-                      <motion.div
-                        key={allUsers.indexOf(user)}
-                        layout
-                        transition={{ type: 'spring', stiffness: 500, damping: 30 }}
-                        style={{ display: 'flex', justifyContent: 'center' }}
+      <div className={isTabletOrMobile ? 'common-container__mobile' : 'common-container'}>
+        {isTabletOrMobile && (
+          <div className="users-tab" onClick={() => setDropdown(!dropdown)}>
+            {dropdown ? 'Hide users' : 'Show users'}
+          </div>
+        )}
+        <div className={isTabletOrMobile && dropdown ? 'chat-users__mobile' : 'chat-users'}>
+          <div className="chat-users__inner">
+            <div className="chat-users__inner-container">
+              <Reorder.Group
+                as="div"
+                axis="y"
+                layoutScroll
+                values={allUsers}
+                onReorder={setAllUsers}
+                style={{ listStyleType: 'none', width: '100%' }}
+              >
+                {allUsers &&
+                  allUsers.map((user) => {
+                    return (
+                      <Reorder.Item
+                        key={user}
+                        value={user}
+                        dragControls={controls}
+                        dragListener={false}
                       >
-                        <UserItem
-                          userName={user}
-                          dispatchChat={dispatchChatState}
-                          online={online.includes(user)}
-                          chat={chatSpeaker}
-                        />
-                      </motion.div>
-                    </Reorder.Item>
-                  );
-                })}
-            </Reorder.Group>
+                        <motion.div
+                          key={allUsers.indexOf(user)}
+                          layout
+                          transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+                          style={{ display: 'flex', justifyContent: 'center' }}
+                        >
+                          <UserItem
+                            userName={user}
+                            dispatchChat={dispatchChatState}
+                            online={online.includes(user)}
+                            chat={chatSpeaker}
+                          />
+                        </motion.div>
+                      </Reorder.Item>
+                    );
+                  })}
+              </Reorder.Group>
+            </div>
           </div>
         </div>
       </div>
