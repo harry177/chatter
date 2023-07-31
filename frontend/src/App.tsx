ee82aacf719729177b4f4c442c6723ea7bc55aa4
Harry177 <harry177@rambler.ru>
2023-07-31 23:30:52 +0200

    feat: implement functions disabling auto-scale on inputs; add black color to buttons text; change chat height to be more responsive on mobile devices

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { MutableRefObject, useEffect, useRef, useState } from 'react';
 import { CommonView } from './Components/CommonView/CommonView';
 import { Header } from './Components/Header/Header';
 import { Footer } from './Components/Footer/Footer';
@@ -10,6 +10,46 @@ export const App = () => {
   const handleDataChange = (data: React.SetStateAction<string>) => {
     setData(data);
   };
+
+  const viewportMeta = useRef() as MutableRefObject<HTMLMetaElement>;
+  let isZoomAllowed = true;
+
+  function handleInputTouchStart(): void {
+    isZoomAllowed = false;
+    viewportMeta.current.setAttribute('user-scalable', 'no');
+  }
+
+  function handleInputTouchEnd(): void {
+    isZoomAllowed = true;
+    viewportMeta.current.setAttribute('user-scalable', 'yes');
+  }
+
+  function handleDocumentTouchMove(event: TouchEvent): void {
+    if (!isZoomAllowed) {
+      event.preventDefault();
+    }
+  }
+
+  useEffect(() => {
+    viewportMeta.current = document.querySelector('meta[name="viewport"]') as HTMLMetaElement;
+
+    const inputs = document.querySelectorAll('input');
+    inputs.forEach((input) => {
+      input.addEventListener('touchstart', handleInputTouchStart);
+      input.addEventListener('touchend', handleInputTouchEnd);
+    });
+
+    document.addEventListener('touchmove', handleDocumentTouchMove, { passive: false });
+
+    return () => {
+      inputs.forEach((input) => {
+        input.removeEventListener('touchstart', handleInputTouchStart);
+        input.removeEventListener('touchend', handleInputTouchEnd);
+      });
+
+      document.removeEventListener('touchmove', handleDocumentTouchMove);
+    };
+  }, []);
 
   return (
     <div className="general">
